d9ab698a385db1c39d33dadd3efa38bdd1c62663
Thunderbolt2812 <gustavopineda95@gmail.com>
2023-05-15 23:39:39 -0600

    Mark 1 Estructura
    Creacion de pagina home

--- client/src/Modules/Home/Home.jsx
+++ client/src/Modules/Home/Home.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import '../HojaDeEstilos/Home.css';
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const navigate = useNavigate();
-    const handlePacientesClick = () => {
-        navigate('/pacientes');
-    };
-    return (  
-        <div>
-            <h1>Home</h1>
-            <button onClick={handlePacientesClick}>Pacientes </button>
+    return (
+        <div className="scrollable-page">
+            <header className="header">
+                <div className="logo">Your Logo</div>
+                <nav>
+                    <div className="buttons">
+                        <button>Inicio</button>
+                        <button>Laboratorio</button>
+                        <button>Iniciar Sesión</button>
+                    </div>
+                </nav>
+            </header>
+            <div className="background-div">
+                Mision
+            </div>
         </div>
-        
     )
 }
 
