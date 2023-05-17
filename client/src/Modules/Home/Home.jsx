734d267fc1f3ebf1130799a17ed4b253a8a3f6ac
Thunderbolt2812 <gustavopineda95@gmail.com>
2023-05-16 20:09:57 -0600

    Mark 2 Estructura
    Se agrego elementos al home

--- client/src/Modules/Home/Home.jsx
+++ client/src/Modules/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className="scrollable-page">
             <header className="header">
-                <div className="logo">Your Logo</div>
+                <div className="logo">Logo</div>
                 <nav>
                     <div className="buttons">
                         <button>Inicio</button>
@@ -16,8 +16,29 @@ const Home = () => {
                     </div>
                 </nav>
             </header>
-            <div className="background-div">
-                Mision
+            <div class="background-div">
+                <div class="contentM">
+                    <h1>Mision</h1>
+                    <p>Contenido</p>
+                </div>
+                <div class="contentV">
+                    <h1>Vision</h1>
+                    <p>Contenido</p>
+                </div>
+            </div>
+            <div className="biography-card">
+                <div className="image-container">
+                </div>
+                <div className="biography-container">
+                    <h2>John Doe</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus massa a turpis
+                        sollicitudin, eget molestie mi gravida. Nulla facilisi. Quisque a neque eu nibh feugiat
+                        viverra vitae in justo. Nunc efficitur lacus nec diam tempus, vel fringilla velit cursus.
+                        Integer ac leo nunc. Sed luctus neque in lacus semper efficitur. Donec efficitur erat id
+                        semper ullamcorper.
+                    </p>
+                </div>
             </div>
         </div>
     )
