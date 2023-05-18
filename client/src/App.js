eb83dce63004d6a7a82fb66ab3d8094341e96c3b
Vjcm16192003 <vjcm13162003@gmail.com>
2023-05-17 21:05:17 -0600

    mark 2.4

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,6 @@ import {
   Routes,
 } from "react-router-dom";
 import Home from "./Modules/Home/Home";
-import Pacientes from "./Modules/Pacientes/Pacientes";
-import AddPacientes from "./Modules/Pacientes/AddPacientes";
-import EditPacientes from "./Modules/Pacientes/EditPacientes";
 import IniciarSesion from "./Modules/Home/IniciarSesion";
 import Laboratorio from "./Modules/Home/Laboratorio";
 import RegistrarUser from "./Modules/Home/RegistrarUser";
@@ -26,9 +23,7 @@ function App() {
           <Route path="/iniciarsesion" element={<IniciarSesion/>}/>
           <Route path="/laboratorio" element={<Laboratorio/>}/>
           <Route path="/registrar-user" element={<RegistrarUser/>}/>
-          <Route path="/pacientes" element={<Pacientes/>}/>
-          <Route path="/pacientes/crear" element={<AddPacientes/>}/>
-          <Route path="/pacientes/:pacienteId" element={<EditPacientes/>}/>
+          
           <Route path="/expedientes" element={<Expedientes/>}/>
           <Route path="/expedientes/crear" element={<AddExpedientes/>}/>
           <Route path="/expedientes/:expedienteId" element={<EditExpedientes/>}/>
