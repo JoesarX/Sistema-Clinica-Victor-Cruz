36fa66d23eb8270249b385bc5a2147a19953c687
Josue Espinal <josueespinalsanz@gmail.com>
2023-05-21 00:56:34 -0600

    v3

--- client/src/Modules/Expedientes/Expedientes.jsx
+++ client/src/Modules/Expedientes/Expedientes.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 import { Box, Button } from '@mui/material'
-import { DataGrid, esES } from '@mui/x-data-grid';
+import { DataGrid, esES, GridActionsCellItem } from '@mui/x-data-grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { GridToolbarContainer, GridToolbarFilterButton, GridToolbarDensitySelector, GridToolbarExport } from '@mui/x-data-grid';
-import { PersonAdd, Edit, Delete } from '@mui/icons-material'
+import { PersonAdd, Edit, Delete , Person, Person2} from '@mui/icons-material'
 import { IconButton } from '@mui/material';
 
 import ExpedientesService from '../../Services/ExpedientesService';
@@ -65,7 +65,7 @@ const Expedientes = () => {
       };
 
       return (
-         <GridToolbarContainer sx={{ display: 'flex', justifyContent: 'space-between' }}>
+         <GridToolbarContainer sx={{ display: 'flex', justifyContent: 'space-between' , height: '30px', marginTop: '15px',marginBottom: '10px'}}>
             <div>
                {/* <GridToolbarColumnsButton /> */}
                <GridToolbarFilterButton />
@@ -73,7 +73,7 @@ const Expedientes = () => {
                <GridToolbarExport />
             </div>
             <div>
-               <Button onClick={handleAgregarExpedienteClick} startIcon={<PersonAdd />}>
+               <Button onClick={handleAgregarExpedienteClick} startIcon={<PersonAdd />} style={{backgroundColor: 'rgb(27,96,241)' , color: 'white', borderRadius: '10px',  paddingLeft: '10px',paddingRight: '10px'}}>
                   Agregar Expediente
                </Button>
             </div>
@@ -111,31 +111,64 @@ const Expedientes = () => {
                      getRowId={(row) => row.pacienteId}
                      columns={[
                         //{ field: 'idpaciente', headerName: 'ID', flex: 1 , headerClassName: 'column-header'},
-                        { field: 'nombre', headerName: 'Nombre', flex: 5 , headerClassName: 'column-header', headerClassName: 'column-header'},
+                        {
+                           field: 'nombre',
+                           headerName: 'Nombre',
+                           flex: 5,
+                           headerClassName: 'column-header',
+                           renderCell: (params) => (
+                             <div style={{ display: 'flex', alignItems: 'center', color: params.field === 'nombre' ? 'black' : 'rgb(121,121,121)'  }}>
+                               {params.row.sexo === 'M' ? (
+                                 <Person style={{ color: '#fff', backgroundColor: 'rgb(26,94,235)', borderRadius: '50%', marginRight: '5px' , fontSize: '200%'}} />
+                               ) : (
+                                 <Person2 style={{ color: '#fff', backgroundColor: 'rgb(236,43,254)', borderRadius: '50%', marginRight: '5px', fontSize: '200%' }} />
+                               )}
+                               {params.value}
+                             </div>
+                           ),
+                         },
                         { field: 'edad', headerName: 'Edad', flex: 1 , headerClassName: 'column-header'},
                         //{ field: 'fecha_nacimiento', headerName: 'Fecha de Nacimiento', flex: 1 , headerClassName: 'column-header'},
                         { field: 'sexo', headerName: 'Sexo', flex: 1 , headerClassName: 'column-header'},
-                        { field: 'correo', headerName: 'Correo', flex: 5 , headerClassName: 'column-header'},
-                        { field: 'telefono', headerName: 'Telefono', flex: 3 , headerClassName: 'column-header'},
-                        { field: 'numid', headerName: 'Numero de Identidad', flex: 4 , headerClassName: 'column-header'},
+                        {
+                           field: 'correo',
+                           headerName: 'Correo Electronico',
+                           flex: 5,
+                           headerClassName: 'column-header',
+                           renderCell: (params) => (
+                             <div style={{ display: 'flex', alignItems: 'center' }}>
+                               {params.value && params.value.includes('@') ? (
+                                 <div style={{ backgroundColor: 'rgb(200,213,255)', color: 'rgb(38,104,242)', padding: '4px 8px', borderRadius: '20px', marginRight: '5px' }}>
+                                   {params.value}
+                                 </div>
+                               ) : (
+                                 params.value
+                               )}
+                             </div>
+                           ),
+                         },
+                        { field: 'telefono', headerName: 'Telefono Celular', flex: 3 , headerClassName: 'column-header'},
+                        { field: 'numid', headerName: 'Num. Identidad', flex: 4 , headerClassName: 'column-header'},
                         //{ field: 'estado_civil', headerName: 'Estado Civil', flex: 1 },
                         //{ field: 'padecimientos', headerName: 'Padecimientos', flex: 1 },
                         //{ field: 'ocupacion', headerName: 'Ocupacion', flex: 1 },
-                        {
+              
+                         {
                            field: 'actions',
                            headerName: '',
                            flex: 2,
                            renderCell: (params) => (
-                              <div>
-                                 <IconButton onClick={() => handleEditExpedientesClick(params.id)}>
-                                    <Edit />
-                                 </IconButton>
-                                 <IconButton onClick={() => handleDeleteExpedientesClick(params.id)}>
-                                    <Delete />
-                                 </IconButton>
-                              </div>
+                             <div>
+                               <IconButton onClick={() => handleEditExpedientesClick(params.id)}>
+                                 <Edit />
+                               </IconButton>
+                               <IconButton onClick={() => handleDeleteExpedientesClick(params.id)}>
+                                 <Delete />
+                               </IconButton>
+                             </div>
                            ),
-                        },
+                         },
+                         
                      ]}
                      components={{
                         Toolbar: CustomToolbar,
@@ -153,4 +186,4 @@ const Expedientes = () => {
 
 }
 
-export default Expedientes
+export default Expedientes 
