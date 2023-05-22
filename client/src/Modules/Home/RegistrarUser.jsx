77f51d0b094367a143d48e99c14febf769e92cb6
aguilar0429 <eaguilaortiz0403@gmail.com>
2023-05-21 21:57:53 -0600

    sign up hecho
    faltaria hacer cosas de firebase y el sign in

--- client/src/Modules/Home/RegistrarUser.jsx
+++ client/src/Modules/Home/RegistrarUser.jsx
@@ -1,113 +1,127 @@
 import React from 'react'
 import '../HojaDeEstilos/RegistrarUser.css';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import UsuariosService from '../../Services/UsuariosService';
 
 
-
-const RegistarUser = () => {
-    const [username, setUsername] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-  };
-    
+const RegistrarUser = () => {
+    const [user, setUser] = React.useState({
+      correouser: '',
+      nombre: '',
+      edad: '',
+      pregunta: '',
+      respuesta: '',
+      password: ''
+    });
     const navigate = useNavigate();
 
-    const handleReturnClick = () => {
-        navigate('/');
+    const handleChange = (e) => {
+      setUser((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
+      console.log(user)
     };
+
+    const handleSubmit = async e => {
+        console.log(user)
+      
+          e.preventDefault();
+          await UsuariosService.postUsuarios(user);
+          alert('User Agregado')
+          navigate('/iniciarsesion');
+      
+  }
+
+    const handleReturnClick = () => {
+      navigate('/');
+
+    };
+
     return (
 
-        <div className="scrollable-page">
-            <header className="header">
-                <div className="logo">Logo</div>
-                <nav>
-                    <div className="buttons">
-                        <button onClick={handleReturnClick}>Volver a Inicio</button>
+      <div className="scrollable-page">
+        <header className="header">
+          <div className="logo">Logo</div>
+          <nav>
+            <div className="buttons">
+              <button onClick={handleReturnClick}>Volver a Inicio</button>
 
-                    </div>
-                </nav>
-            </header>
+            </div>
+          </nav>
+        </header>
 
 
-            <div className="register-form">
-      <h2>Registrar Usuario </h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Nombre de Usuario: </label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+        <div className="register-form">
+          <h2>Registrar Usuario </h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="correo">Correo: </label>
+              <input
+                type="text"
+               
+               
+                onChange={handleChange}
+                name = "correouser"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstName">Nombre Completo:</label>
+              <input type="text"  onChange={handleChange} name='nombre' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Edad:</label>
+              <input
+                type="number"
+                id="edad"
+                
+                onChange={handleChange}
+                name='edad'
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pregunta">Pregunta:</label>
+              <select
+                type="text"
+                id="pregunta"
+                
+                onChange={handleChange}
+                name='pregunta'
+                required
+              >
+                <option value='Primera Mascota'>Nombre Primera Mascota</option>
+                <option value='Nombre Madre'>Nombre de madre</option>
+                <option value='Nombre Padre'>Nombre de padre</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="respuesta">Respuesta: </label>
+              <input
+                type="respuesta"
+                id="respuesta"
+                name='respuesta'
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Constraseña: </label>
+              <input
+                type="password"
+                id="password"
+                name='password'
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit} >Registrar Usuario: </button>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="firstName">Primer Nombre:</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName">Primer Apellido:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">E-mail:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña: </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirmar Constraseña: </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Registrar Usuario: </button>
-      </form>
-    </div>
 
-        </div>
+      </div>
 
 
     )
+  
 }
 
-export default RegistarUser
+export default RegistrarUser
---
+++ server/routes/usuarios.js
@@ -0,0 +1,48 @@
+import express from "express";
+const router = express.Router();
+
+const usuariosRouter = (pool) => {
+    //get all users
+    router.get("/usuarios", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            const sqlSelect = "SELECT * FROM usuarios ";
+            const [rows, fields] = await connection.query(sqlSelect);
+            connection.release();
+            res.json(rows);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+
+   
+
+
+
+    //Add a new user
+    router.post("/usuarios", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            const q =
+                "INSERT INTO `usuarios` (`correouser`, `nombre`, `edad`, `pregunta`, `respuesta`, `password`) VALUES (?)";
+            const values = [
+                req.body.correouser,
+                req.body.nombre,
+                req.body.edad,
+                req.body.pregunta,
+                req.body.respuesta,
+                req.body.password
+            ];
+            await connection.query(q, [values]);
+            connection.release();
+            res.json("Usuario añadido exitosamente!");
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+    return router;
+};
+
+export default usuariosRouter;
