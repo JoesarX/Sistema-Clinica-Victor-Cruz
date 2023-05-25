f0801cf34620d5a560d3e81d4edd65e31bf44451
Vjcm16192003 <vjcm13162003@gmail.com>
2023-05-24 22:33:05 -0600

    About Us cases terminado

--- client/src/Modules/Home/Acercade.jsx
+++ client/src/Modules/Home/Acercade.jsx
@@ -5,6 +5,14 @@ import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import ReactDOM from 'react-dom';
 import { useState } from 'react';
+import doctor from '../Imagenes/doctor.jpg';
+import hospital from '../Imagenes/hospital.jpeg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Acercade = () => {
 
@@ -12,25 +20,25 @@ const Acercade = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const navigate = useNavigate();
   const handleReturnClick = () => {
-      navigate('/');
+    navigate('/');
   };
   const handleIniciarClick = () => {
-      navigate('/iniciarsesion');
+    navigate('/iniciarsesion');
   };
   const handleCitaClick = () => {
-      navigate('/citas');
+    navigate('/citas');
   };
   const handleLabClick = () => {
-      navigate('/laboratorio');
+    navigate('/laboratorio');
   };
 
 
   const handleServicios = () => {
-      navigate('/servicios');
+    navigate('/servicios');
   };
 
   const handleAcercade = () => {
-      navigate('/acerca-de');
+    navigate('/acerca-de');
   };
 
   function mostrarBotones1() {
@@ -44,18 +52,18 @@ const Acercade = () => {
     boton1.style.top = '30px';
     boton2.style.visibility = 'visible';
     boton2.style.top = '60px';
-}
+  }
 
-function ocultarBotones1() {
+  function ocultarBotones1() {
     var boton1 = document.getElementById('bt1');
     var boton2 = document.getElementById('bt2');
     var div2 = document.getElementById('mi1');
     div2.style.display = 'none';
     boton1.style.visibility = 'hidden';
     boton2.style.visibility = 'hidden';
-}
+  }
 
-function mostrarBotones2() {
+  function mostrarBotones2() {
     var boton1 = document.getElementById('bt3');
     var boton2 = document.getElementById('bt4');
     var div1 = document.getElementById('m2');
@@ -66,18 +74,18 @@ function mostrarBotones2() {
     boton1.style.top = '30px';
     boton2.style.visibility = 'visible';
     boton2.style.top = '60px';
-}
+  }
 
-function ocultarBotones2() {
+  function ocultarBotones2() {
     var boton1 = document.getElementById('bt3');
     var boton2 = document.getElementById('bt4');
     var div2 = document.getElementById('mi2');
     div2.style.display = 'none';
     boton1.style.visibility = 'hidden';
     boton2.style.visibility = 'hidden';
-}
+  }
 
-window.onload = function () {
+  window.onload = function () {
     ocultarBotones1();
     ocultarBotones2();
     var botonPrincipal = document.getElementById('btp1');
@@ -86,53 +94,115 @@ window.onload = function () {
     botonPrincipal.addEventListener('mouseleave', ocultarBotones1);
     botonPrincipal2.addEventListener('mouseenter', mostrarBotones2);
     botonPrincipal2.addEventListener('mouseleave', ocultarBotones2);
-};
+  };
 
 
   const properties = {
-      duration: 3000,
-      transitionDuration: 500,
-      infinite: true,
-      indicators: true,
-      arrows: true
+    duration: 3000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true
   };
 
 
 
 
   const toggleDropdown = () => {
-      setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
 
 
   return (
-        <div className="scrollable-page">
-            <header className="headerT">
-                <button className="bt" style={{ fontSize: '18px' }} onClick={handleReturnClick}>INICIO</button>
-                <button className="bt" style={{ fontSize: '18px' }} onClick={handleLabClick}>LABORATORIO</button>
-                <button className="bt" style={{ fontSize: '18px' }} onClick={handleIniciarClick}>INICIAR SESIÓN</button>
-                <button className="bt1" style={{ fontSize: '18px' }} id="btp1" onMouseEnter={mostrarBotones1}
-                    onMouseLeave={ocultarBotones1}>ACERCA DE
-                    <div className="menu_desplegable" id='m1'>
-                        <div className="menu_item" id='mi1'>
-                            <button className='bt_menu' id='bt1' onClick={handleAcercade}>Sobre Nosotros</button>
-                            <button className='bt_menu' id='bt2' onClick={handleAcercade}>Contáctanos</button>
-                        </div>
-                    </div>
-                </button>
-                <button className="bt2" style={{ fontSize: '18px' }} id="btp2" onMouseEnter={mostrarBotones2}
-                    onMouseLeave={ocultarBotones2}>SERVICIOS
-                    <div className="menu_desplegable" id='m2'>
-                        <div className="menu_item" id='mi2'>
-                            <button className='bt_menu' id='bt3' onClick={handleServicios}>Servicios</button>
-                            <button className='bt_menu' id='bt4' onClick={handleServicios}>Otros Servicios</button>
-                        </div>
-                    </div>
-                </button>
-            </header>
-            
+    <div className="scrollable-page">
+      <header className="headerT">
+        <button className="bt" style={{ fontSize: '18px' }} onClick={handleReturnClick}>INICIO</button>
+        <button className="bt" style={{ fontSize: '18px' }} onClick={handleLabClick}>LABORATORIO</button>
+        <button className="bt" style={{ fontSize: '18px' }} onClick={handleIniciarClick}>INICIAR SESIÓN</button>
+        <button className="bt1" style={{ fontSize: '18px' }} id="btp1" onMouseEnter={mostrarBotones1}
+          onMouseLeave={ocultarBotones1}>ACERCA DE
+          <div className="menu_desplegable" id='m1'>
+            <div className="menu_item" id='mi1'>
+              <button className='bt_menu' id='bt1' onClick={handleAcercade}>Nosotros</button>
+              <button className='bt_menu' id='bt2' onClick={handleAcercade}>Contáctanos</button>
+            </div>
+          </div>
+        </button>
+        <button className="bt2" style={{ fontSize: '18px' }} id="btp2" onMouseEnter={mostrarBotones2}
+          onMouseLeave={ocultarBotones2}>SERVICIOS
+          <div className="menu_desplegable" id='m2'>
+            <div className="menu_item" id='mi2'>
+              <button className='bt_menu' id='bt3' onClick={handleServicios}>Servicios</button>
+              <button className='bt_menu' id='bt4' onClick={handleServicios}>Otros Servicios</button>
+            </div>
+          </div>
+        </button>
+      </header>
+
+      <div className="about-us-container">
+        <h1>About Us</h1>
+
+        <div className="mission-vision-container">
+          <div className="mission">
+            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px', fontSize: '40px'}}>
+              <FontAwesomeIcon icon={faBullseye} />
+            </div>
+            <h2>Mission</h2>
+            <p>Nuestra misión en nuestra clínica médica y laboratorio de análisis clínicos es
+              proporcionar atención médica de alta calidad a nuestros pacientes, con énfasis en
+              la prevención, el diagnóstico y el tratamiento de enfermedades. Estamos dedicados
+              a proporcionar atención médica individualizada, segura y eficiente mediante la
+              utilización de tecnologías.</p>
+          </div>
+
+          <div className="vision">
+            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px', fontSize: '40px' }}>
+              <FontAwesomeIcon icon={faEye} />
+            </div>
+            <h2>Vision</h2>
+            <p>Ser conocido como la clínica médica y laboratorio de análisis clínicos que garantice
+              su excelencia en el servicio al paciente, la precisión de sus diagnósticos y la
+              atención personalizada de su personal de expertos altamente calificados.</p>
+          </div>
         </div>
+
+        <div className="company-description">
+          <div className="image-container">
+            <img src={hospital} />
+          </div>
+          <div className="text-container">
+            <h2>Company Description</h2>
+            <p>Our company is a leading provider of cutting-edge technology solutions. We specialize in developing software applications and providing consulting services to clients across various industries. With a focus on delivering exceptional value and driving digital transformation, we help businesses achieve their goals and stay ahead in today's competitive market.</p>
+          </div>
+        </div>
+
+        <div className="person-container">
+          <div className="person-image">
+            <img src={doctor} />
+          </div>
+          <div className="person-description">
+            <h2>John Doe</h2>
+            <p>John Doe is the founder and CEO of our company. With over 20 years of experience in the technology sector, he has led our organization to numerous successes. John is passionate about driving innovation and creating a positive impact through technology.</p>
+          </div>
+        </div>
+
+        <div className="employee-description">
+          <h2>Our Team</h2>
+          <p>We have a team of dedicated professionals who are experts in their respective fields. From software engineers to designers and project managers, our team works collaboratively to deliver outstanding results for our clients. With a shared commitment to excellence, we constantly strive to exceed expectations and push the boundaries of what's possible.</p>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <p style={{ color: '#fff' }}>  <FontAwesomeIcon icon={faLocationDot} /> Col.Kennedy, Tegucigalpa</p>
+        <p style={{ color: '#fff' }}>  <FontAwesomeIcon icon={faPhone} /> (504) 2228-3233</p>
+        <button className='botonCon' >
+          Contactanos para responder tus dudas
+        </button>
+      </footer>
+
+    </div>
+
   );
 };
 
