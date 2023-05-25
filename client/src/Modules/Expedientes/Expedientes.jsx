3c1a640ea1f346819762941483fd838a64829372
aguilar0429 <eaguilaortiz0403@gmail.com>
2023-05-24 21:00:52 -0600

    agregar done

--- client/src/Modules/Expedientes/Expedientes.jsx
+++ client/src/Modules/Expedientes/Expedientes.jsx
@@ -124,12 +124,38 @@ const Expedientes = () => {
 
    const handleModalFieldChange = (e) => {
       setExpediente((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
-      console.log(expediente)
+      
    }
+
+   const calculateAge = (dob) => {
+      const birthDate = new Date(dob);
+      const today = new Date();
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
+    };
+   const handleDateChange = (date) => {
+
+      setFechaNacimiento(date);
+
+      const formattedDate = date ? date.toISOString().slice(0, 10) : '';
+    
+      setExpediente((prevState) => ({ ...prevState, fecha_nacimiento: formattedDate }))
+     
+
+   };
+   const [fecha_nacimiento, setFechaNacimiento] = useState(null);
 
    const handleModalSubmit = async (e) => {
       e.preventDefault();
+      const age = fecha_nacimiento ? calculateAge(fecha_nacimiento) : '';
+      setExpediente((prevState) => ({ ...prevState, edad: age }))
+      
       setIsSubmitting(true);
+     
 
       if (validations()) {
          try {
@@ -143,7 +169,7 @@ const Expedientes = () => {
          }
       }
    };
-
+   const [estado_civil, setSelectedOption] = useState(null);
    const validations = () => {
       const { nombre, edad, fecha_nacimiento, sexo, correo, telefono, numid, estado_civil, padecimientos, ocupacion } = expediente
       if (nombre === null || nombre === '') {
@@ -174,7 +200,7 @@ const Expedientes = () => {
       }
       return true
    }
-
+   
    useEffect(() => {
       //validación login
       if (!isLoggedIn) {
@@ -199,7 +225,6 @@ const Expedientes = () => {
          fetchAllExpedientes();
       }
    }, [isSubmitting]);
-
 
    return (
       <div className='expedientesGrid'>
@@ -290,39 +315,46 @@ const Expedientes = () => {
                      noValidate
                      autoComplete="off"
                   >
-                     <TextField id="nombre" label="Nombre Completo" variant="outlined" />
+                     <TextField id="nombre" label="Nombre Completo" variant="outlined" onChange={handleModalFieldChange} name='nombre' />
                      <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                              <DatePicker id="fecha_nacimiento" label="Fecha de Nacimiento" />
+                              <DatePicker id="fecha_nacimiento" label="Fecha de Nacimiento" value={fecha_nacimiento || null} onChange={handleDateChange} name='fecha_nacimiento' />
                            </LocalizationProvider>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                           <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" id='sexo'>
+                           <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" id='sexo' onChange={handleModalFieldChange} name="sexo" >
                               <FormControlLabel value="F" control={<Radio />} label="Femenino" />
                               <FormControlLabel value="M" control={<Radio />} label="Masculino" />
                            </RadioGroup>
                         </Grid>
                      </Grid>
-                     <TextField id="ocupacion" label="Ocupación" variant="outlined" />
+                     <TextField id="ocupacion" label="Ocupación" variant="outlined" onChange={handleModalFieldChange} name='ocupacion' />
                      <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
-                           <TextField id="correo" label="Correo Electrónico" variant="outlined" type='email' />
+                           <TextField id="correo" label="Correo Electrónico" variant="outlined" type='email' onChange={handleModalFieldChange} name='correo' />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                           <TextField id="telefono" label="Teléfono" variant="outlined" />
+                           <TextField id="telefono" label="Teléfono" variant="outlined" onChange={handleModalFieldChange} name='telefono' />
                         </Grid>
                      </Grid>
                      <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
-                           <TextField id="numid" label="Número de Identidad" variant="outlined" type='number' />
+                           <TextField id="numid" label="Número de Identidad" variant="outlined" type='number' onChange={handleModalFieldChange} name='numid' />
                         </Grid>
                         <Grid item xs={12} sm={6}>
                            <Autocomplete
                               disablePortal
                               id="estado_civil"
                               options={listaEstadoCivil}
+                              onChange={(event, newValue) =>
+                                 setExpediente({
+                                   ...expediente,
+                                   estado_civil: newValue
+                                 })
+                               }
                               renderInput={(params) => <TextField {...params} label="Estado Civil" />}
+
                            />
                         </Grid>
                      </Grid>
