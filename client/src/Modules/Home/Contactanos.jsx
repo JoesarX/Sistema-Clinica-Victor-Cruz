ac47d645c7196f307da4c8a8e644181ed83657f2
Ana Romero <73078714+AnaCRomero@users.noreply.github.com>
2023-05-28 17:59:45 -0600

    cambios sobre Contactanos

--- client/src/Modules/Home/Contactanos.jsx
+++ client/src/Modules/Home/Contactanos.jsx
@@ -16,41 +16,41 @@ const Contactanos = () => {
     };
 
     return (
-        <div >
+        <div>
             <Topbar />
-            <section id="contact" class="d-flex flex-column justify-content-center align-items-center">
-                <div class="container text-center text-md-left" data-aos="fade-up">
+            <section id="contact" className="d-flex flex-column justify-content-center align-items-center">
+                <div className="container text-center text-md-left" data-aos="fade-up">
                     <h1>Contáctanos</h1>
                 </div>
-                <div class="container text-center text-md-left" data-aos="fade-up">
+                <div className="container text-center text-md-left" data-aos="fade-up">
                     <h2>¿Tienes alguna duda? Puedes entrar en contacto con nosotros y con gusto te ayudamos.</h2>
                 </div>
             </section>
-            <h4 className='subtitle'>Puedes contactarte con nosostros por los siguientes medios.</h4>
+            <h4 className="subtitle">Puedes contactarte con nosotros por los siguientes medios.</h4>
             <section className="medios">
                 <div className="cont">
                     <div className="box">
-                        <h4>Llamada telefónica</h4>
-                        <p>Puedes llamarnos al número</p>
-                        <FontAwesomeIcon icon={faPhone} />
-                        <span> +504 2230-3901</span>
+                        <h4 style={{ color: '#1E60A6' }}>Llamada telefónica</h4>
+                        <p style={{ color: '#1E60A6' }}>Puedes llamarnos al número</p>
+                        <FontAwesomeIcon icon={faPhone} style={{ color: '#1560F2' }} />
+                        <span style={{ color: '#1E60A6', fontWeight: 'bold' }}> +504 2230-3901</span>
                     </div>
                     <div className="box" onClick={openWhatsApp}>
-                        <h4>WhastApp</h4>
-                        <p>Puedes escribirnos al número</p>
-                        <FontAwesomeIcon icon={faWhatsapp} />
-                        <span> +504 3342-4985</span>
+                        <h4 style={{ color: '#1E60A6' }}>WhatsApp</h4>
+                        <p style={{ color: '#1E60A6' }}>Puedes escribirnos al número</p>
+                        <FontAwesomeIcon icon={faWhatsapp} style={{ fontSize: '19px', color: '#1560F2' }} />
+                        <span style={{ color: '#1E60A6', fontWeight: 'bold' }}> +504 3342-4985</span>
                     </div>
                     <div className="box" onClick={openEmail}>
-                        <h4>Correo Electrónico</h4>
-                        <p>Puedes escribirnos al correo</p>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                        <span> clinica.drvictorcruz@gmail.com</span>
+                        <h4 style={{ color: '#1E60A6' }}>Correo Electrónico</h4>
+                        <p style={{ color: '#1E60A6' }}>Puedes escribirnos al correo</p>
+                        <FontAwesomeIcon icon={faEnvelope} style={{ color: '#1560F2' }} />
+                        <span style={{ color: '#1E60A6', fontWeight: 'bold' }}> clinica.drvictorcruz@gmail.com</span>
                     </div>
                 </div>
             </section>
             <Footer />
-        </div >
+        </div>
     );
 };
 
