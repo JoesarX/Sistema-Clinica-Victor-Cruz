ff9c92c35b07c045befdcebd2c2d945cd3c369e1
Josue Espinal <josueespinalsanz@gmail.com>
2023-06-07 15:08:08 -0600

    Revert "Merge branch 'main' into Backend-Medicamentos"
    This reverts commit ef776a7b05eb299bb0a14f2931642dc2377ca31a, reversing
    changes made to 81311c4fbb9ce3f0d7d10f4ebeaffdc66233295f.

---
+++ client/src/Services/MedicamentosService.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000';
+// const API_URL = 'https://clinicavictorcruzserver.azurewebsites.net';
+
+export const getAllMedicamentos = async () => {
+    try {
+        const res = await axios.get(`${API_URL}/medicamentos`);
+        console.log("SUCCESS FETCHING MEDICAMENTOS");
+        console.log(res.data);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch medicamentos');
+    }
+};
+
+export const getOneMedicamento = async (id) => {
+    console.log(id);
+    try {
+        const res = await axios.get(`${API_URL}/medicamentos/${id}`);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch medicamento');
+    }
+};
+
+export const postMedicamentos = async (medicamento) => {
+    try {
+        const res = await axios.post(`${API_URL}/medicamentos`, medicamento);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to post medicamento');
+    }
+};
+
+export const editMedicamentos = async (id,medicamento) => {
+    try {
+        await axios.put(`${API_URL}/medicamentos/${id}`,medicamento);
+       
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to edit medicamento');
+    }
+};
+
+export const deleteMedicamentos = async (id) => {
+    try {
+         await axios.delete(`${API_URL}/medicamentos/${id}`);
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to delete medicamento');
+    }
+};
+
+const Services ={
+    getAllMedicamentos,
+    postMedicamentos,
+    getOneMedicamento,
+    deleteMedicamentos,
+    editMedicamentos
+    // Other functions
+};
+
+export default Services;
---
+++ server/routes/medicamentos.js
@@ -0,0 +1,113 @@
+import express from "express";
+
+const router = express.Router();
+
+const medicamentosRouter = (pool) => {
+    
+    //Get all patients
+    router.get("/", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            // const sqlSelect = "SELECT * FROM medicamentos ";
+            const sqlSelect = "SELECT * FROM medicamentos"
+            const [rows, fields] = await connection.query(sqlSelect);
+            connection.release();
+            res.json(rows);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+
+    //Add a new medicamento
+    router.post("/", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            const q =
+                "INSERT INTO `medicamentos` (`nombre`, `categoria`, `stock`, `precio_unitario`, `via`, `dosis`)  VALUES (?)";
+            const values = [
+                req.body.nombre,
+                req.body.categoria,
+                req.body.stock,
+                req.body.precio_unitario,
+                req.body.via,
+                req.body.dosis
+            ];
+            await connection.query(q, [values]);
+            connection.release();
+            res.json("Medicamento añadido exitosamente!");
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+
+    //Get a medicamento by id
+    router.get("/:id", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+           
+            const sqlSelect = "SELECT * FROM medicamentos WHERE idmed = " + req.params.id;
+          
+            const [rows, fields] = await connection.query(sqlSelect);
+            connection.release();
+            res.json(rows[0])
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+
+    //Delete a patient by id
+    router.delete("/:id", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            const sqlSelect = "delete FROM medicamentos where idmed = " + req.params.id;
+            const [rows, fields] = await connection.query(sqlSelect);
+            connection.release();
+            res.json(rows);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+
+    //Update a patient by id
+    router.put("/:id", async (req, res) => {
+        try {
+            const connection = await pool.getConnection();
+            const { id } = req.params;
+            const {
+                nombre,
+                categoria,
+                stock,
+                precio_unitario,
+                via,
+                dosis
+            } = req.body;
+
+            const q =
+                "UPDATE medicamentos SET nombre = ?, categoria = ?, stock = ?, precio_unitario = ?, via = ?, dosis = ? WHERE idmed = ?";
+
+            const values = [
+                nombre,
+                categoria,
+                stock,
+                precio_unitario,
+                via,
+                dosis,
+                id
+            ];
+
+            await connection.query(q, values);
+            connection.release();
+            res.json("Medicamento actualizado exitosamente!");
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+    return router;
+};
+
+export default medicamentosRouter;
