333a47ab0ac6f837f517b7c9c368f06ce852e5a0
Ana Romero <73078714+AnaCRomero@users.noreply.github.com>
2023-06-19 19:29:19 -0600

    implementación del calendario

--- client/src/Modules/Home/Citas.jsx
+++ client/src/Modules/Home/Citas.jsx
@@ -1,93 +1,42 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 import '../HojaDeEstilos/Citas.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import esLocale from '@fullcalendar/core/locales/es';
+
+import Topbar from './Topbar';
+import Footer from './Footer';
+
 
 const Citas = () => {
-  const [selectedSlot, setSelectedSlot] = useState(null);
-  const [selectedDay, setSelectedDay] = useState(moment().format('YYYY-MM-DD'));
-  const [email, setEmail] = useState('');
 
-  const handleSlotClick = (slot) => {
-    if (!selectedSlot && slot.available) {
-      setSelectedSlot(slot);
-    }
-  };
-
-  const handleConfirmAppointment = () => {
-    if (selectedSlot && email) {
-      // Perform necessary actions here (e.g., send confirmation email, update database, etc.)
-      alert(`Cita Confirmada! Hora:${selectedSlot.time} el ${selectedSlot.day} para ${email}`);
-
-      // Mark the slot as booked
-      const updatedSlot = { ...selectedSlot, available: false };
-      setSelectedSlot(updatedSlot);
-
-      // Clear the selected slot and email
-      setSelectedSlot(null);
-      setEmail('');
-    }
-  };
-
-  const handleDayChange = (e) => {
-    setSelectedDay(e.target.value);
-    setSelectedSlot(null); // Reset the selected slot when the day changes
-  };
-
-  const renderSlots = () => {
-    const slots = [];
-    const startTime = moment('08:00 AM', 'hh:mm A');
-    const endTime = moment('05:00 PM', 'hh:mm A');
-
-    while (startTime.isBefore(endTime)) {
-      const slot = {
-        time: startTime.format('hh:mm A'),
-        day: selectedDay,
-        available: true,
-      };
-
-      slots.push(
-        <div
-          key={`${slot.day}-${slot.time}`}
-          className={`slot ${selectedSlot === slot ? 'selected' : ''} ${slot.available ? 'available' : 'booked'}`}
-          onClick={() => handleSlotClick(slot)}
-          style={{ backgroundColor: selectedSlot === slot ? 'grey' : (slot.available ? 'inherit' : 'red') }}
-        >
-          {slot.time}
-        </div>
-      );
-
-      startTime.add(1, 'hour');
-    }
-
-    return slots;
-  };
+  const hiddenDays = [0, 6];
 
   return (
     <div className="App">
-      <h1>Appointment Calendar</h1>
-      <div className="calendar">
-        <div className="day-selector">
-          <label htmlFor="day">Select a day:</label>
-          <input
-            type="date"
-            id="day"
-            value={selectedDay}
-            onChange={handleDayChange}
-          />
-        </div>
-        <div className="time-slots">{renderSlots()}</div>
-        <div className="appointment-form">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button onClick={handleConfirmAppointment}>Confirm Appointment</button>
-        </div>
+      <Topbar />
+      <div className='header'>
+        NUESTRA DISPONIBILIDAD
       </div>
+  
+      <div className='cal'>
+        <FullCalendar
+          headerToolbar={{
+            center: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          hiddenDays={hiddenDays}
+          locale={esLocale}
+        />
+      </div>
+      <Footer />
     </div>
   );
+  
 };
 
 export default Citas;
