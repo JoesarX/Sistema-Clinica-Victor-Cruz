001b97f7ea02456bec8a9986197f95d4396417ba
Vjcm16192003 <vjcm13162003@gmail.com>
2023-06-24 14:38:09 -0600

    Little Update

--- client/src/Modules/Citas_files/Citas_Doc.jsx
+++ client/src/Modules/Citas_files/Citas_Doc.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CitasStyle.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -6,12 +6,25 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
 import NavBar from '../NavBar';
-
 import Footer from '../Home/Footer';
-
+import CitasService from '../../Services/CitasService';
 
 const Citas_Doc = () => {
   const hiddenDays = [1, 7];
+  const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    obtenerCitas();
+  }, []);
+
+  const obtenerCitas = async () => {
+    try {
+      const citasData = await CitasService.getAllCitas();
+      setCitas(citasData);
+    } catch (error) {
+      console.error('Error al obtener las citas:', error);
+    }
+  };
 
   return (
     <div className="App">
@@ -25,7 +38,6 @@ const Citas_Doc = () => {
                 left: 'prev,next today',
                 center: 'title',
                 right: 'dayGridMonth,timeGridWeek,timeGridDay',
-
               }}
               plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
               initialView="dayGridMonth"
@@ -45,6 +57,12 @@ const Citas_Doc = () => {
               slotMinTime="07:00:00"
               slotMaxTime="17:00:00"
               allDaySlot={false}
+              events={citas.map(cita => ({
+                title: `Expediente: ${cita.expediente}`,
+                start: cita.fechaInicio,
+                end: cita.fechaFin,
+                description: `Hora de inicio: ${cita.horaInicio}, Hora final: ${cita.horaFin}`,
+              }))}
             />
           </div>
         </div>
@@ -56,4 +74,5 @@ const Citas_Doc = () => {
     </div>
   );
 };
+
 export default Citas_Doc;
