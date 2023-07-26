e2dc279701784a021268c33e5388df32f43b1e32
Vjcm16192003 <vjcm13162003@gmail.com>
2023-07-25 22:12:58 -0600

    Task Finished

--- client/src/Modules/AuthContext.js
+++ client/src/Modules/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useRef } from 'react';
 
 const AuthContext = createContext();
 
@@ -22,13 +22,44 @@ const AuthProvider = ({ children }) => {
     setUserType(userType);
     localStorage.setItem('isLoggedIn', true);
     localStorage.setItem('userType', userType);
+    resetAutoLogoutTimer();
   };
 
   const handleSignOut = () => {
     setIsLoggedIn(false);
     setUserType('normal');
     localStorage.clear();
+    window.location.reload()
   };
+
+  // Auto-logout timer
+  const autoLogoutTime = 1 * 10 * 1000; // 5 minutes in milliseconds
+  const timerIdRef = useRef();
+
+  const resetAutoLogoutTimer = () => {
+    clearTimeout(timerIdRef.current);
+    timerIdRef.current = setTimeout(() => {
+      handleSignOut();
+      alert('You have been logged out due to inactivity.');
+    }, autoLogoutTime);
+  };
+
+  const handleUserActivity = () => {
+    resetAutoLogoutTimer();
+  };
+
+  useEffect(() => {
+    // Set up event listeners for user activity
+    window.addEventListener('mousedown', handleUserActivity);
+    window.addEventListener('keydown', handleUserActivity);
+
+    return () => {
+      // Clean up event listeners when the component unmounts
+      window.removeEventListener('mousedown', handleUserActivity);
+      window.removeEventListener('keydown', handleUserActivity);
+      clearTimeout(timerIdRef.current);
+    };
+  }, []);
 
   const contextValue = {
     isLoggedIn,
