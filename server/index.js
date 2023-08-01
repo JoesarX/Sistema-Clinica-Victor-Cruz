52c05b5fc5201ce9c1215902e2dd459936812aa2
WalterReyes1 <walter.reyes@cambridgehn.com>
2023-07-31 21:44:10 -0600

    Actualizando ya para db

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import adminRouter from "./routes/usuarios_admin.js"
 import medicamentosRouter from "./routes/medicamentos.js";
 import citasRouter from "./routes/citas.js";
 import categoriesRouter from "./routes/categories.js";
-
+import textos_cmdRouter from "./routes/textos_cmd.js";
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -54,6 +54,8 @@ app.use("/medicamentos", medicamentosRouter(pool));
 app.use("/citas", citasRouter(pool)); 
 app.use("/medicamentos", medicamentosRouter(pool)); // Pass the pool object as a parameter
 app.use("/categorias", categoriesRouter(pool)); // Pass the pool object as a parameter
+
+app.use('/texto_cmd',textos_cmdRouter(pool));
 
 
 
