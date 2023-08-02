614bb21fc11ea0451bdaedffb223b0718b0fcbc2
Josue Espinal <josueespinalsanz@gmail.com>
2023-08-02 15:00:51 -0600

    Prueba Azure Email 3:30

--- server/routes/citas.js
+++ server/routes/citas.js
@@ -297,7 +297,7 @@ const citasRouter = (pool, transporter) => {
         const millisecondsInADay = 24 * 60 * 60 * 1000;
         const now = new Date();
         const targetTime = new Date(now);
-        targetTime.setHours(16, 55, 0, 0); // Aqui se puede cambiar la hora a la que se mandan los correos
+        targetTime.setHours(15, 30, 0, 0); // Aqui se puede cambiar la hora a la que se mandan los correos
 
         let timeUntilNextDay = targetTime - now;
         if (timeUntilNextDay < 0) {
