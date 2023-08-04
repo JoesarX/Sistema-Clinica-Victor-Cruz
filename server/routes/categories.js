75858e2a21bef4994e3164c3b85ef25c71ea6a77
Josue Espinal <josueespinalsanz@gmail.com>
2023-08-03 20:15:03 -0600

    categorias console log fix

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -9,7 +9,7 @@ const categoriesRouter = (pool) => {
             const sqlSelect = "SELECT * FROM categorias ";
             const [rows, fields] = await connection.query(sqlSelect);
             connection.release();
-            Console.log("Get all categories Successfull");
+            console.log("Get all categories Successfull");
             res.json(rows);
         } catch (err) {
             console.log("Get all categories Failed. Error: " + err);
@@ -24,14 +24,14 @@ const categoriesRouter = (pool) => {
             const connection = await pool.getConnection();
             const q =
                 "INSERT INTO `categorias` (`Nombre_Categoria`) VALUES (?)";
-           
-            await connection.query(q,req.body[0]);
+
+            await connection.query(q, req.body[0]);
             connection.release();
             console.log("Post category Successfull");
             res.json("Categoria añadida exitosamente!");
         } catch (err) {
             console.log("Post category Failed. Error: " + err);
-            res.status(500).json({ error: "Internal Server Error"});
+            res.status(500).json({ error: "Internal Server Error" });
         }
     });
     //delete Category
@@ -41,10 +41,10 @@ const categoriesRouter = (pool) => {
             const sqlSelect = "delete FROM categorias where id = '" + req.params.id + "'";
             const [rows, fields] = await connection.query(sqlSelect);
             connection.release();
-            Console.log("Delete category "+ req.params.id+ " Successfull");
+            Console.log("Delete category " + req.params.id + " Successfull");
             res.json(rows);
         } catch (err) {
-            console.log("Delete category "+ req.params.id+ " Failed. Error: " + err);
+            console.log("Delete category " + req.params.id + " Failed. Error: " + err);
             res.status(500).json({ error: "Internal Server Error" });
         }
     });
@@ -53,30 +53,30 @@ const categoriesRouter = (pool) => {
     router.put("/:id", async (req, res) => {
         try {
             const connection = await pool.getConnection();
-            const {id}  = req.params;
-           // console.log(id);
-           const {
-            editedValue
-           }=req.body;
+            const { id } = req.params;
+            // console.log(id);
+            const {
+                editedValue
+            } = req.body;
             const q =
                 "UPDATE categorias SET Nombre_Categoria = ? WHERE ID = ?";
-          
+
             console.log(req.body[1] + " " + req.body[0]);
-           // console.log(req);
+            // console.log(req);
             const values = [
                 req.body[0],
                 req.body[1]
-                
+
             ];
-            
-           /* console.log(values);
-            console.log(values[0]);
-            console.log(values[1]);*/
-           await connection.query(q, values);
+
+            /* console.log(values);
+             console.log(values[0]);
+             console.log(values[1]);*/
+            await connection.query(q, values);
             connection.release();
             console.log(`Put category ${id} Successfull`);
             res.json("Categoría actualizado exitosamente!");
-          
+
         } catch (err) {
             console.log(`Put category ${id} Failed. Error: ` + err);
             res.status(500).json({ error: "Internal Server Error" });
