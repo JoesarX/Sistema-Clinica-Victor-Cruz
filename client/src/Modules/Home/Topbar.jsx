1379762fd1d38a0d286d6ccdf7369c2b0942e6b1
Thunderbolt2812 <gustavopineda95@gmail.com>
2023-08-16 17:56:47 -0600

    Cambio a textos topbar
    Ya arregle los textps del top bar

--- client/src/Modules/Home/Topbar.jsx
+++ client/src/Modules/Home/Topbar.jsx
@@ -102,7 +102,7 @@ const Topbar = () => {
                         <div>
                             {userType === 'normal' && (
                                 // Content for normal user dropdown menu
-                                <li className="nav-item dropdown" style={{ width: '160px' }}>
+                                <li className="nav-item-dropdown" style={{ width: '160px' }}>
                                     <div className="d-flex justify-content-end">
                                         <div className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         <span style={{fontSize: '18px'}}>{nombre}</span>
@@ -118,8 +118,7 @@ const Topbar = () => {
                             )}
 
                             {userType === 'administrador' && (
-                                // Content for administrator dropdown menu
-                                <li className="nav-item dropdown" style={{ width: '160px' }}>
+                                <li className="nav-item-dropdown" style={{ width: '160px' }}>
                                     <div className="d-flex justify-content-end">
                                         <div className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         <span style={{fontSize: '18px'}}>{nombre}</span>
@@ -133,7 +132,7 @@ const Topbar = () => {
                             )}
                             {userType === 'master' && (
                                 // Content for master user dropdown menu
-                                <li className="nav-item dropdown" style={{ width: '160px' }}>
+                                <li className="nav-item-dropdown" style={{ width: '140px' }}>
                                     <div className="d-flex justify-content-end">
                                         <div className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             <span style={{fontSize: '18px'}}>{nombre}</span>
