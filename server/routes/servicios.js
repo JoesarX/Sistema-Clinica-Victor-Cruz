1f6d4d16315042f59e8feab2314df67a70b177df
adrianburgosf <adrianburgosf@gmail.com>
2023-08-15 21:27:18 -0600

    backup
    bu

--- server/routes/servicios.js
+++ server/routes/servicios.js
@@ -8,7 +8,7 @@ const serviciosRouter = (pool) => {
     router.get("/", async (req, res) => {
         try {
             const connection = await pool.getConnection();
-            const sqlSelect = "SELECT id, url, title, description FROM servicios"
+            const sqlSelect = "SELECT id, url, title, description, orden, visibility FROM servicios order by orden asc"
             const [rows, fields] = await connection.query(sqlSelect);
             connection.release();
             res.json(rows);
@@ -23,11 +23,13 @@ const serviciosRouter = (pool) => {
         try {
             const connection = await pool.getConnection();
             const q =
-                "INSERT INTO `servicios` (`url`, `title`, `description`)  VALUES (?)";
+                "INSERT INTO `servicios` (`url`, `title`, `description`, `orden`, `visibility`)  VALUES (?)";
             const values = [
                 req.body.url,
                 req.body.title,
-                req.body.description
+                req.body.description,
+                req.body.orden,
+                req.body.visibility
             ];
             await connection.query(q, [values]);
             connection.release();
@@ -60,8 +62,12 @@ const serviciosRouter = (pool) => {
             const connection = await pool.getConnection();
             const sqlSelect = "delete FROM servicios where id = " + req.params.id;
             const [rows, fields] = await connection.query(sqlSelect);
+            const updateSql = "UPDATE servicios SET orden = orden - 1 WHERE orden > ?";
+            const updateParams = [req.body.orden];
+            const [updateResult] = await connection.query(updateSql, updateParams);
             connection.release();
-            res.json(rows);
+            //res.json(rows);
+            res.json({ rows, updateResult });
         } catch (err) {
             console.log(err);
             res.status(500).json({ error: "Internal Server Error" });
@@ -78,14 +84,18 @@ const serviciosRouter = (pool) => {
                 url,
                 title,
                 description,
+                orden,
+                visibility,
             } = req.body;
             const q =
-                "UPDATE servicios SET url = ?, title = ?, description = ? WHERE id = ?";
+                "UPDATE servicios SET url = ?, title = ?, description = ?, orden = ?, visibility = ? WHERE id = ?";
 
             const values = [
                 url,
                 title,
                 description,
+                orden,
+                visibility,
                 id
             ];
 
