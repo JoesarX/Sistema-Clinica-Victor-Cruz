1bb76c94f98eb2db93c5645f0c55e0f91340f7ca
Vjcm16192003 <vjcm13162003@gmail.com>
2023-08-19 16:14:57 -0600

    Logica Cotizacion Done

--- client/src/Modules/Home/Laboratorio.jsx
+++ client/src/Modules/Home/Laboratorio.jsx
@@ -1,21 +1,15 @@
-
-import React from 'react'
+import React, { useState } from 'react';
 import '../HojaDeEstilos/Laboratorio.css';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-
 import Topbar from './Topbar';
 import Footer from './Footer';
 
-
 const Laboratorio = () => {
+    
     const [cartItems, setCartItems] = useState([]);
-    const [selectedExams, setSelectedExams] = useState([]);
+    const [quotingEnabled, setQuotingEnabled] = useState(false);
 
     const navigate = useNavigate();
-    const handleCotizarClick = () => {
-        navigate('laboratorio/cotizacion-examen');
-    };
 
     const handleVolverClick = () => {
         navigate('/');
@@ -41,11 +35,14 @@ const Laboratorio = () => {
         setCartItems(updatedItems.filter((item) => item.quantity > 0));
     };
 
-    const handleTransaction = () => {
-        // Handle the transaction logic here
-        // You can access the selected exams from the 'selectedExams' state array
-        console.log('Transaction completed:', selectedExams);
+    const handleToggleQuoting = () => {
+        if (quotingEnabled) {
+            setCartItems([]);
+        }
+        setQuotingEnabled(!quotingEnabled);
     };
+
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
     const exams = [
         {
@@ -95,45 +92,21 @@ const Laboratorio = () => {
         // Add more lab exams here
     ];
 
-    const toggleSelection = (examId) => {
-        setSelectedExams((prevSelectedExams) => {
-            if (prevSelectedExams.includes(examId)) {
-                return prevSelectedExams.filter((id) => id !== examId);
-            } else {
-                return [...prevSelectedExams, examId];
-            }
-        });
-    };
-
     return (
         <div className="scrollable-page1">
-
-
             <Topbar />
-
             <div className='header'>
-                CATALAGO DE EXAMENES
+                CATALOGO DE EXAMENES
             </div>
             <div className="empty-space-top"></div>
             <div className="catalog-container">
                 {exams.map((exam) => (
                     <div className="exam-card" key={exam.id}>
-                        <div className="selection">
-                            <input
-                                type="checkbox"
-                                id={`checkbox-${exam.id}`}
-                                checked={selectedExams.includes(exam.id)}
-                                onChange={() => toggleSelection(exam.id)}
-                                className="custom-checkbox"
-                            />
-                            <label htmlFor={`checkbox-${exam.id}`} className="checkbox-label"></label>
-                        </div>
-
                         <h2>{exam.name}</h2>
-                        <p>{exam.description}</p>
+                        <p className="description">{exam.description}</p>
                         <div className="price">{`Precio: Lps.${exam.price}`}</div>
                         <div className="quantity">
-                            <span>Unidadades: </span>
+                            <span>Unidades: </span>
                             <button onClick={() => decreaseQuantity(exam)}>-</button>
                             <span>{cartItems.find((item) => item.id === exam.id)?.quantity || 0}</span>
                             <button onClick={() => addToCart(exam)}>+</button>
@@ -141,22 +114,27 @@ const Laboratorio = () => {
                     </div>
                 ))}
             </div>
-
-            {selectedExams.length > 0 && (
-                <div className="transaction-container">
-                    <button className="transaction-button" onClick={handleTransaction}>
-                        Complete Transaction
-                    </button>
+            <div className="transaction-container">
+                <button className="transaction-button" onClick={handleToggleQuoting}>
+                    {quotingEnabled ? "Cancelar Cotización" : "Cotizar Exámenes"}
+                </button>
+            </div>
+            {quotingEnabled && (
+                <div className="quoting-container">
+                    <h3>Cotización</h3>
+                    {cartItems.map((item) => (
+                        <div key={item.id} className="quoting-item">
+                            <span className="quoting-item-name">{item.name}</span>
+                            <span className="quoting-item-price">{`Precio: Lps.${item.price * item.quantity}`}</span>
+                        </div>
+                    ))}
+                    <div className="total-amount">{`Total: Lps.${totalAmount}`}</div>
                 </div>
             )}
-
             <div className="empty-space-bottom"></div>
             <Footer />
         </div>
     );
 };
-
-
-
 
 export default Laboratorio;
