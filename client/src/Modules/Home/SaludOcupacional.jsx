b81f6c51be75bccea31be6f6eb2ec32cc2a4a240
Vjcm16192003 <vjcm13162003@gmail.com>
2023-08-29 13:48:07 -0600

    first draft Salud Ocupacional

--- client/src/Modules/Home/SaludOcupacional.jsx
+++ client/src/Modules/Home/SaludOcupacional.jsx
@@ -5,6 +5,7 @@ import Topbar from './Topbar';
 import Footer from './Footer';
 
 import { useEffect, useCallback } from 'react'
+import DoctorImg from '../Imagenes/doctorPhoto.jpg'
 
 const SaludOcupacional = () => {
 
@@ -15,7 +16,7 @@ const SaludOcupacional = () => {
         <div className="scrollable-page2">
             <Topbar />
             <div className='header1'>
-                CATALOGO DE EXAMENES
+                SALUD OCUPACIONAL
             </div>
             <div className="empty-space-top1"></div>
 
@@ -30,12 +31,16 @@ const SaludOcupacional = () => {
             </div>
 
             <div className="salud_impor-container">
-                <h2>¿Qué es la Salud Ocupacional?</h2>
+                <h2>¿Por qué es importante la salud ocupacional?</h2>
                 <p>
-                    se define como una actividad multidisciplinaria que controla y realiza medidas de prevención para cuidar la salud de todos los trabajadores.
-                    Esto incluye enfermedades, cualquier tipo de accidentes y todos los factores que puedan llegar a poner en peligro la vida,
-                    la salud o la seguridad de las personas en sus respectivos trabajos.
+                    Una de las principales razones para poner atención en la salud ocupacional es la
+                    reducción en el porcentaje de ausentismo debido a la baja de enfermedades o malestares y el incremento de la felicidad.
                 </p>
+            </div>
+
+
+            <div className="person-container1">
+                
             </div>
 
 
