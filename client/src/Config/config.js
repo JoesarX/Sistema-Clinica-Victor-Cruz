d954f641c27e837c65931968968eb6e5cd4e03e3
adrianburgosf <adrianburgosf@gmail.com>
2023-09-06 16:30:58 -0600

    newpaypalacc
    npa

--- client/src/Config/config.js
+++ client/src/Config/config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID || "AeLQpEh9ZdoDDPYOptHxp0rnt5_dqCL9M-xtggZjfWEIKtTZngqwhEEJBG_hNQElI5diC-YKQ4IHi6ME"
-export const APP_SECRET = process.env.APP_SECRET || "EEGFhgrQl2M463nwM0yevG3pFWzEhj82HtYoInGHwaBtacj0b5svCz14ORm6xzKJVB9XLyuD09lgpcWh"
+export const CLIENT_ID = process.env.CLIENT_ID || "AeR9pcSmW9rFk0Uom5dxSRQb_axfztJsB2zcc91jBzAR9KWwxgKdVxrEn-wAPV5DnbxWAEV6ticM1bP7"
+export const APP_SECRET = process.env.APP_SECRET || "EANpTAQd-hbF7F6ZOfRbHwK1pqtnFH77yOGl2QNi4Ib3Dv8FDa4p3BJLduOYGHE8A_CBaxdWa6sV0d8h"
