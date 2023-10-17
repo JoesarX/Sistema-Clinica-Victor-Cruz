e12ceafc87b3364dcdc97d73f4e1a597fb15917c
Josue Espinal <josueespinalsanz@gmail.com>
2023-10-17 00:36:51 -0600

    link change azure

--- client/src/Services/ServiciosService.js
+++ client/src/Services/ServiciosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:8000';
-const API_URL = 'https://clinicadrvictorcruz.azurewebsites.net';
+const API_URL = 'https://DrVictorCruz.azurewebsites.net';
 
 export const getAllServicios = async () => {
     try {
